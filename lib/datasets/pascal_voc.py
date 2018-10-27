b0c02e004bcd480a01671603578fe18740b85ac0
huan <huan@huan.local>
2018-10-27 22:20:54 +0800

    将voc训练集中只选出来人的图片库做为训练时查找图片的目录，如果是测试集val则从全部图片库中去查找，如果是训练集则从筛选出来一定含有人的图片的图片库去查找

--- lib/datasets/pascal_voc.py
+++ lib/datasets/pascal_voc.py
@@ -75,8 +75,15 @@ class pascal_voc(imdb):
     """
     Construct an image path from the image's "index" identifier.
     """
-    image_path = os.path.join(self._data_path, 'JPEGImages',
-                              index + self._image_ext)
+    #todo 更换目录测试是不是只使用含有人的图片去训练
+    # image_path = os.path.join(self._data_path, 'JPEGImages',
+    #                           index + self._image_ext)
+    if self._image_set == "trainval":
+      image_path = os.path.join(self._data_path, 'PersonJPEGImages',
+                                index + self._image_ext)
+    else:
+      image_path = os.path.join(self._data_path, 'JPEGImages',
+                                index + self._image_ext)
     assert os.path.exists(image_path), \
       'Path does not exist: {}'.format(image_path)
     return image_path
@@ -263,54 +270,7 @@ class pascal_voc(imdb):
             'flipped': False,
             'seg_areas': seg_areas}
 
-  # def _load_pascal_annotation(self, index):
-  #   """
-  #   Load image and bounding boxes info from XML file in the PASCAL VOC
-  #   format.
-  #   """
-  #   #记载xml文件
-  #   filename = os.path.join(self._data_path, 'Annotations', index + '.xml')
-  #   tree = ET.parse(filename)
-  #   objs = tree.findall('object')
-  #   if not self.config['use_diff']:
-  #     # Exclude the samples labeled as difficult
-  #     non_diff_objs = [
-  #       obj for obj in objs if int(obj.find('difficult').text) == 0]
-  #     # if len(non_diff_objs) != len(objs):
-  #     #     print 'Removed {} difficult objects'.format(
-  #     #         len(objs) - len(non_diff_objs))
-  #     objs = non_diff_objs
-  #   num_objs = len(objs)
-  #
-  #   boxes = np.zeros((num_objs, 4), dtype=np.uint16)
-  #   gt_classes = np.zeros((num_objs), dtype=np.int32)
-  #   overlaps = np.zeros((num_objs, self.num_classes), dtype=np.float32)
-  #   # "Seg" area for pascal is just the box area
-  #   seg_areas = np.zeros((num_objs), dtype=np.float32)
-  #
-  #   # Load object bounding boxes into a data frame.
-  #   for ix, obj in enumerate(objs):
-  #     bbox = obj.find('bndbox')
-  #     #todo zjia pban
-  #     name = obj.find('name')
-  #     if name.text == 'person':
-  #       # Make pixel indexes 0-based
-  #       x1 = float(bbox.find('xmin').text) - 1
-  #       y1 = float(bbox.find('ymin').text) - 1
-  #       x2 = float(bbox.find('xmax').text) - 1
-  #       y2 = float(bbox.find('ymax').text) - 1
-  #       cls = self._class_to_ind[obj.find('name').text.lower().strip()]
-  #       boxes[ix, :] = [x1, y1, x2, y2]
-  #       gt_classes[ix] = cls
-  #       overlaps[ix, cls] = 1.0
-  #       seg_areas[ix] = (x2 - x1 + 1) * (y2 - y1 + 1)
-  #   overlaps = scipy.sparse.csr_matrix(overlaps)
-  #
-  #   return {'boxes': boxes,
-  #           'gt_classes': gt_classes,
-  #           'gt_overlaps': overlaps,
-  #           'flipped': False,
-  #           'seg_areas': seg_areas}
+
 
 
   def _get_comp_id(self):
